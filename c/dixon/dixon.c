8b4c5f00d434c3344bf935be0db482b4eac89026
Tristan Delcourt <tristandelcourt826@gmail.com>
2024-06-21 09:03:42 +0200

    triangulating matrix

--- c/dixon/dixon.c
+++ c/dixon/dixon.c
@@ -2,14 +2,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gmp.h>
+#include <assert.h>
 
 void print_list(int* l, int n){
     for(int i = 0; i<n; i++){
         printf("%d ", l[i]);
     }
+    printf("\n");
 }
 
-bool factorise(mpz_t n, int* vi, int piB){
+void print_ll(int** ll, int n1, int n2){
+    for(int i = 0; i<n1; i++){
+        print_list(ll[i], n2);
+    }
+    printf("\n");
+}
+
+bool isPrime(int n) 
+{ 
+    // Corner cases 
+    if (n <= 1) 
+        return false; 
+    if (n <= 3) 
+        return true; 
+  
+    // This is checked so that we can skip 
+    // middle five numbers in below loop 
+    if (n % 2 == 0 || n % 3 == 0) 
+        return false; 
+  
+    for (int i = 5; i * i <= n; i = i + 6) 
+        if (n % i == 0 || n % (i + 2) == 0) 
+            return false; 
+  
+    return true; 
+} 
+  
+int pi(int n) { 
+    int k = 0;
+    for (int i = 2; i <= n; i++) { 
+        if (isPrime(i)) 
+            k++;; 
+    } 
+    return k;
+} 
+
+int* primes(int piB, int n){
+    int* p = malloc(piB*sizeof(piB));
+    int k = 0;
+    for (int i = 2; i <= n; i++) { 
+        if (isPrime(i)){
+            p[k] = i;
+            k++;
+        }
+    }
+    return p;
+
+}
+
+
+/* DIXON */
+
+bool factorise(mpz_t n, int* v, int piB, int* primes){
+    for(int i = 0; i<piB; i++){
+        v[i] = 0;
+    }
+    
+    for(int i = 0; i<piB && mpz_cmp_ui(n, 1); i++){
+        while (mpz_congruent_ui_p(n, 0, primes[i])){
+            mpz_divexact_ui(n, n, primes[i]);
+            v[i]++;
+        }
+    }
+
+    if(!mpz_cmp_ui(n, 1))
+        return true;
+    return false;
+      
+    /*
     for(int i = 0; i<piB; i++){
         vi[i] = 0;
     }
@@ -39,9 +109,11 @@ bool factorise(mpz_t n, int* vi, int piB){
     }
 
     return true;
+    */
 }
 
-int* get_zi(mpz_t zi, mpz_t N, int piB, gmp_randstate_t rstate){
+/*
+int* get_vi(mpz_t zi, mpz_t N, int piB, mpz_t x){
     bool found = false;
     int* vi = malloc(piB*sizeof(int));
 
@@ -57,45 +129,130 @@ int* get_zi(mpz_t zi, mpz_t N, int piB, gmp_randstate_t rstate){
 
     return vi;
 }
+*/
 
 
-int** get_all_zi(mpz_t* z, mpz_t N, int piB){
-    gmp_randstate_t rstate;
-    //unsigned long seed;
-    gmp_randinit_default(rstate);
-    //gmp_randseed_ui(rstate, seed);
+int** get_all_zi(mpz_t* z, mpz_t N, int piB, int* primes){
+    mpz_t x;
+    mpz_init(x);
+    mpz_root(x, N, 2);
+    mpz_t x_2_n;
+    mpz_init(x_2_n);
 
     int** v = malloc((piB+1)*sizeof(int*));
+
     for(int i = 0; i < piB+1; i++){
-        v[i] = get_zi(z[i], N, piB, rstate);  
+        bool found = false;
+        int* vi = malloc(piB*sizeof(int));
+
+        while(!found){
+            mpz_add_ui(x, x, 1);
+            mpz_mul(x_2_n, x, x);
+            mpz_sub(x_2_n, x_2_n, N);
+            //gmp_printf("%Zd\n", x_2_n);
+            found = factorise(x_2_n, vi, piB, primes);
+        }
+
+        v[i] = vi;
+        mpz_set(z[i], x);
+        //gmp_printf("%Zd\n", z[i]);
+        //print_list(vi, piB);
+        //printf("\n\n");
     }
 
     return v;
 }
 
-void dixon(mpz_t N, int piB){
+void swap_lines(int** v, int i, int j){
+    int* temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+
+int find_index(int** v, int from, int piB){
+    for(int i = from; i < piB+1; i++){
+        if(v[i][from]){
+            return i;
+        }
+    }
+    return -1;
+}
+
+unsigned modulo( int value, unsigned m) {
+    int mod = value % (int)m;
+    if (mod < 0) {
+        mod += m;
+    }
+    return mod;
+}
+
+void mod_vect(int* v, int mod, int piB){
+    for(int i = 0; i<piB; i++){
+        v[i] = modulo(v[i], mod);
+    }
+}
+
+void sub_vect(int** v, int i, int j, int piB){
+    for(int k = 0; k<piB; k++){
+        v[i][k] = v[i][k] - v[j][k];
+    }
+}
+
+int* gaussian_solve(int** v, int piB){
+    printf("Initial vectors\n");
+    print_ll(v, piB+1, piB);
+    
+    for(int i = 0; i<piB+1; i++){
+        mod_vect(v[i], 2, piB);
+    }
+
+    printf("Modded\n");
+    print_ll(v, piB+1, piB);
+
+    for(int i = 0; i<piB; i++){
+        int k = find_index(v, i, piB);
+        if(k != -1){
+            swap_lines(v, i, k);
+
+            for(int j = i + 1; j < piB+1; j++){
+                if(v[j][i] == 1){
+                    sub_vect(v, j, i, piB);
+                    mod_vect(v[j], 2, piB);
+                }
+            }
+        }
+    }
+    printf("Triangulate\n");
+    print_ll(v, piB+1, piB);
+
+    int* sol = malloc((piB+1)*sizeof(int));
+
+
+
+}
+
+void dixon(mpz_t N, int B){
+    int piB = pi(B);
+    int* p = primes(piB, B);
+    //print_list(p, piB);
 
     mpz_t* z = malloc((piB+1)*sizeof(mpz_t));
     for(int i = 0; i < piB +1; i++){
         mpz_init(z[i]);
     }
 
-    int** v = get_all_zi(z, N, piB);
-    for(int i = 0; i < piB+1; i++){
-        gmp_printf("%zd\n", z[i]);
-        print_list(v[i], piB);
-        printf("\n");
-    }
+    int** v = get_all_zi(z, N, piB, p);
+
+    int* solution = gaussian_solve(v, piB);
 
 }
 
-void main(int agrc, char**argv){
-    int B = 10000;
-    int piB = 168;
+void main(int argc, char**argv){
+    assert(argc == 2);
+
+    int B = 50;
     mpz_t N;
     mpz_init_set_str(N, argv[1], 10);
 
-    dixon(N, piB);
-    
-
+    dixon(N, B);
 }
