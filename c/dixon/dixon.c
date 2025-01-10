71056555e75b3ae178797d7788f7aeae8329e51e
Tristan Delcourt <tristandelcourt826@gmail.com>
2025-01-10 22:51:30 +0100

    no more leaks, added extra vectors instead of just +1

--- c/dixon/dixon.c
+++ c/dixon/dixon.c
@@ -5,6 +5,8 @@
 #include <assert.h>
 #include <time.h>
 
+//#define MUL_KARATSUBA_THRESHHOLD 10
+
 void print_list(int* l, int n){
     for(int i = 0; i<n; i++){
         printf("%d ", l[i]);
@@ -19,8 +21,14 @@ void print_ll(int** ll, int n1, int n2){
     printf("\n");
 }
 
-bool isPrime(int n) 
-{ 
+void free_ll(int** m, int n1, int n2){
+    for(int i = 0; i<n1; i++){
+        free(m[i]);
+    }
+    free(m);
+}
+
+bool isPrime(int n) { 
     // Corner cases 
     if (n <= 1) 
         return false; 
@@ -49,7 +57,7 @@ int pi(int n) {
 } 
 
 int* primes(int piB, int B){
-    int* p = malloc(piB*sizeof(piB));
+    int* p = malloc(piB*sizeof(int));
     int k = 0;
     for (int i = 2; i <= B; i++) { 
         if (isPrime(i)){
@@ -75,16 +83,24 @@ int* prime_base(mpz_t n, int* pb_len, int* primes, int piB){
     pb[0] = 2;
 
     int j = 1;
+    mpz_t r, p1;
+    mpz_inits(r, p1, NULL);
     for(int i = 1; i<piB; i++){
-        if(euler_criterion(n, primes[i])){
+        int e = (primes[i]-1)/2;
+        mpz_set_ui(p1, primes[i]);
+        mpz_powm_ui(r, n, e, p1);
+        if(mpz_cmp_ui(r, 1) == 0){
+            //printf("%d %d\n", primes[i], j);
             pb[j] = primes[i];
             j++;
         }
     }
 
-    //printf("base reduction %f percent\n", (float)j/piB*100);
+    printf("base reduction %f%%\n", (float)j/piB*100);
     *pb_len = j;
-    pb = realloc(pb, j);
+    pb = realloc(pb, j*sizeof(int));
+    
+    mpz_clears(r, p1, NULL);
     return pb;
 }
 
@@ -144,10 +160,10 @@ void rebuild(mpz_t prod, int* v, int* primes, int n1){
         mpz_ui_pow_ui(temp, primes[i], v[i]);
         mpz_mul(prod, prod, temp);
     }
+    mpz_clear(temp);
 }
 
-int* sum_lignes(int** v, int n1, int n2, int* sol){
-    int* sum = malloc(n1*sizeof(int));
+int* sum_lignes(int* sum, int** v, int n1, int n2, int* sol){
 
     for(int i = 0; i<n2; i++){
         sum[i] = 0;
@@ -193,7 +209,7 @@ bool factorise(mpz_t n, int* v, int pb_len, int* pb){
     return false;
 }
 
-int** get_all_zi(mpz_t* z, mpz_t N, int pb_len, int* pb){
+int** get_all_zi(mpz_t* z, mpz_t N, int pb_len, int* pb, int extra){
     //ceil(sqrt(n))
     mpz_t sqrt_n;
     mpz_init(sqrt_n);
@@ -207,9 +223,9 @@ int** get_all_zi(mpz_t* z, mpz_t N, int pb_len, int* pb){
     mpz_t qx;
     mpz_inits(zi, qx, NULL);
 
-    int** v = malloc((pb_len+1)*sizeof(int*));
+    int** v = malloc((pb_len+extra)*sizeof(int*));
 
-    for(int i = 0; i < pb_len+1; i++){
+    for(int i = 0; i < pb_len+extra; i++){
         bool found = false;
         int* vi = malloc(pb_len*sizeof(int));
 
@@ -219,23 +235,31 @@ int** get_all_zi(mpz_t* z, mpz_t N, int pb_len, int* pb){
             //Q(x)
             mpz_mul(qx, zi, zi);
             mpz_sub(qx, qx, N);
+
             found = factorise(qx, vi, pb_len, pb);
 
             mpz_add_ui(x, x, 1);
         }
-
+        printf("\r");
+        printf("%f%%", (float)i/(pb_len+extra-1)*100);
+        fflush(stdout);
+        
         v[i] = vi;
         mpz_set(z[i], zi);
         //gmp_printf("%Zd\n", z[i]);
         //print_list(vi, pb_len);
         //printf("\n\n");
     }
+    printf("\n");
+
+    mpz_clears(sqrt_n, zi, x, qx, NULL);
+
 
     return v;
 }
 
 
-int* gaussian_solve(int** v, int n1, int n2){
+int* gaussian_solve(int* sol, int** v, int n1, int n2/*, int* arbitrary_indices, int* len*/){
     //printf("Initial vectors\n");
     //print_ll(v, n1, n2);
     
@@ -268,10 +292,14 @@ int* gaussian_solve(int** v, int n1, int n2){
     //printf("Triangulate\n");
     //print_ll(m, n2, n1);
 
-    int* sol = malloc(n1*sizeof(int));
     for(int i = 0; i<n1; i++){
-        sol[i] =-1;
+        sol[i] = -1;
     }
+
+    
+    //arbitrary_indices = malloc(n1*sizeof(int));
+    //int nb_arb = 0;
+
 
     for(int i = n2-1; i>-1; i--){
         int j = 0;
@@ -284,6 +312,8 @@ int* gaussian_solve(int** v, int n1, int n2){
 
             for(int k = n1-1; k>j; k--){
                 if(sol[k] == -1){
+                    //arbitrary_indices[nb_arb++] = k;
+                    //sol[k] = 0;
                     sol[k] = rand() % 2;
                 }
                 sol[j] -= m[i][k] * sol[k];
@@ -292,62 +322,110 @@ int* gaussian_solve(int** v, int n1, int n2){
         }
     }
 
+    
     for(int i = 0; i<n1; i++){
         if(sol[i] == -1){
+            //arbitrary_indices[nb_arb++] = i;
+            //sol[i] = 0;
             sol[i] = rand() % 2;
         }
     }
-    return sol;
+
+    /*
+    arbitrary_indices = realloc(arbitrary_indices, nb_arb*sizeof(int));
+    print_list(arbitrary_indices, nb_arb);
+    *len = nb_arb;
+    */
+
+    free_ll(m, n2, n1);
 }
 
-void dixon(mpz_t N, int B){
+/*
+bool gen_solutions(int* sol, int* indices, int n){
+    int i = 0;
+    while(i<n && (sol[indices[i]] == 1)){
+        sol[indices[i]] = 0;
+        i++;   
+    }
+    if(i >= n){
+        return true;
+    }
+    sol[indices[i]] = 1;
+    return false;
+}
+*/
+
+void dixon(mpz_t N, int B, int extra){
     int piB = pi(B);
+    printf("pi(B) = %d\n", piB);
     int* p = primes(piB, B);
 
     int pb_len;
     int* pb = prime_base(N, &pb_len, p, piB);
-    //print_list(pb, pb_len);
+    free(p);
 
-    mpz_t* z = malloc((pb_len+1)*sizeof(mpz_t));
-    for(int i = 0; i < pb_len +1; i++){
+    mpz_t* z = malloc((pb_len+extra)*sizeof(mpz_t));
+    for(int i = 0; i < pb_len+extra; i++){
         mpz_init(z[i]);
     }
-
+    
     //Getting zis
     clock_t t1 = clock();
-    int** v = get_all_zi(z, N, pb_len, pb);
+    int** v = get_all_zi(z, N, pb_len, pb, extra);
     clock_t t2 = clock();
     double time_spent = (double)(t2 - t1) / CLOCKS_PER_SEC;
     printf("Time to get zi: %fs\n", time_spent);
 
-    //Solving matrix
-    t1 = clock();
-    int* solution = gaussian_solve(v, pb_len+1, pb_len);
-    t2 = clock();
-    time_spent = (double)(t2 - t1) / CLOCKS_PER_SEC;
-    printf("Time to get solve matrix: %fs\n", time_spent);
+    //int* arbitrary_indices;
+    //int len;
+    
+    mpz_t f1, f2, Z1, Z2;
+    mpz_inits(f1, f2, Z1, Z2, NULL);
+    int* solution = malloc((pb_len+extra)*sizeof(int));
+    int* sum = malloc((pb_len+extra)*sizeof(int));
 
+    bool done = false;
+    int tries = 0;
+    while(!done){
 
-    int* sum = sum_lignes(v, pb_len+1, pb_len, solution);
-    div_vect(sum, 2, pb_len);
+        gaussian_solve(solution, v, pb_len+extra, pb_len);
 
-    mpz_t Z1, Z2;
-    mpz_inits(Z1, Z2, NULL);
-    prod_vect(Z1, z, pb_len+1);
-    rebuild(Z2, sum, p, pb_len);
+        sum_lignes(sum, v, pb_len+extra, pb_len, solution);
+        div_vect(sum, 2, pb_len);
 
-    mpz_t f1, f2;
-    mpz_inits(f1, f2, NULL);
-    mpz_sub(f1, Z1, Z2);
-    mpz_add(f2, Z1, Z2);
+        prod_vect(Z1, z, pb_len+extra);
+        rebuild(Z2, sum, pb, pb_len);
 
-    mpz_gcd(f1, f1, N);
-    mpz_gcd(f2, f2, N);
+        mpz_sub(f1, Z1, Z2);
+        mpz_add(f2, Z1, Z2);
+
+        mpz_gcd(f1, f1, N);
+        mpz_gcd(f2, f2, N);
+
+        if((!(mpz_cmp_ui(f1, 1) == 0) && !(mpz_cmp(f1, N) == 0))
+            || (!(mpz_cmp_ui(f2, 1) == 0) && (!(mpz_cmp(f2, N) == 0)))){
+            done = true;
+        }
+        printf("\r");
+        printf("%d tries", tries);
+        fflush(stdout);
+        tries++;
+    }
+    free(solution);
+    free(sum);
+    free(pb);
+    free_ll(v, pb_len+extra, pb_len);
+    for(int i = 0; i < pb_len+extra; i++){
+        mpz_clear(z[i]);
+    }
+    free(z);
 
     gmp_printf("%Zd = 0 [%Zd]\n%Zd = 0 [%Zd]\n", N, f1, N, f2);
 
     assert(mpz_divisible_p(N, f1));
     assert(mpz_divisible_p(N, f2));
+
+    mpz_clears(f1, f2, Z1, Z2, NULL);
 }
 
 
@@ -355,9 +433,15 @@ void main(int argc, char**argv){
     assert(argc == 2);
     srand(time(NULL));
 
-    int B = 1000;
+    int B = 150;
     mpz_t N;
     mpz_init_set_str(N, argv[1], 10);
 
-    dixon(N, B);
+    clock_t t1 = clock();
+    dixon(N, B, 10);
+    clock_t t2 = clock();
+    double time_spent = (double)(t2 - t1) / CLOCKS_PER_SEC;
+    printf("Total time: %fs\n", time_spent);
+
+    mpz_clear(N);
 }
